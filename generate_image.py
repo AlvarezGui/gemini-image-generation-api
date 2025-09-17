7132f29e578876481b070fcac2953c7f102e5282
Guilherme Alvarez <guilherme_alvarez18@hotmail.com>
2025-09-16 23:34:49 -0300

    feat: envia prompt para a api da ia

--- generate_image.py
+++ generate_image.py
@@ -17,15 +17,15 @@ prompt = (
 
 print(prompt)
 
-# Remove a imagem local
-# response = client.models.generate_content(
-#     model="gemini-2.5-flash-image-preview",
-#     contents=[prompt],
-# )
+# remove a imagem local
+response = client.models.generate_content(
+    model="gemini-2.5-flash-image-preview",
+    contents=[prompt],
+)
 
-# for part in response.candidates[0].content.parts:
-#     if part.text is not None:
-#         print(part.text)
-#     elif part.inline_data is not None:
-#         image = Image.open(BytesIO(part.inline_data.data))
-#         image.save("generated_image.png")
+for part in response.candidates[0].content.parts:
+    if part.text is not None:
+        print(part.text)
+    elif part.inline_data is not None:
+        image = Image.open(BytesIO(part.inline_data.data))
+        image.save("generated_image.png")
