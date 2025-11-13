d10b2df8d4f0ed576ba22f7317eaa2cf774c118e
Guilherme Alvarez <guilherme_alvarez18@hotmail.com>
2025-11-13 19:23:41 -0300

    feat: the docs are now online!

--- main.py
+++ main.py
@@ -9,6 +9,7 @@ from bson import ObjectId
 import json
 from AuthService.AuthController import auth_bp
 from functions.generate_image import generate_image
+from swagger.swagger_blueprint import swagger_bp
 
 
 app = Flask(__name__)
@@ -116,6 +117,7 @@ def api_delete_user(email):
     return jsonify({"mensagem": "Usuário deletado"})
 
 app.register_blueprint(auth_bp)
+app.register_blueprint(swagger_bp)
 
 if __name__ == '__main__':
     app.run(host="127.0.0.1", port=5000, debug=True)
